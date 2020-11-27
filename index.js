b686b10c13427dbc9eb11016564c31e2a39ff52b
nghiatcanhtran <nghiacanhtran@gmail.com>
2020-11-27 11:23:07 +0700

    update style

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function () {
-  $("#date_instance").datepicker({});
-  $("#date_instance").datepicker("setDate", calculate.setDate());
+  $("#date_instance").datepicker({
+    language: "vi",
+    multidate: true,
+  });
+  $("#date_instance").datepicker("setDates", calculate.getArrDate());
 });
 
 var calculate = (function () {
@@ -15,15 +18,49 @@ var calculate = (function () {
     return Math.round((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
   };
 
-  let setDate = () => {
-    var tomorrow = new Date();
-    let distanceWeek = getWeeksBetween(new Date(), config().dateRoot) - 1;
-    let distanceDate = distanceWeek * config().numberBetweenTwoWeek;
-    tomorrow.setDate(config().dateRoot.getDate() + distanceDate);
-    return tomorrow;
+  let getMondays = () => {
+    var d = new Date(),
+      month = d.getMonth(),
+      mondays = [];
+
+    d.setDate(1);
+
+    // Get the first Monday in the month
+    while (d.getDay() !== 1) {
+      d.setDate(d.getDate() + 1);
+    }
+
+    // Get all the other Mondays in the month
+    while (d.getMonth() === month) {
+      mondays.push(new Date(d.getTime()));
+      d.setDate(d.getDate() + 7);
+    }
+
+    return mondays;
+  };
+
+  let getArrDate = () => {
+    var listMonDays = getMondays();
+    var arrDayPick = [];
+    listMonDays.forEach((element) => {
+      let distanceWeek = getWeeksBetween(element, config().dateRoot) - 1;
+      let distanceDate = distanceWeek * config().numberBetweenTwoWeek;
+
+      element.setDate(config().dateRoot.getDate() + distanceDate);
+      arrDayPick.push(element);
+    });
+
+    // var tomorrow = new Date();
+    // let distanceWeek = getWeeksBetween(new Date(), config().dateRoot) - 1;
+    // let distanceDate = distanceWeek * config().numberBetweenTwoWeek;
+    // tomorrow.setDate(config().dateRoot.getDate() + distanceDate);
+    // return tomorrow;
+    console.log(arrDayPick);
+    return arrDayPick;
   };
 
   return {
-    setDate: setDate,
+    getArrDate: getArrDate,
+    getMondays: getMondays,
   };
 })();
--- vi_language.js
+++ vi_language.js
@@ -1,38 +1,10 @@
-jQuery(function ($) {
-  $.datepicker.regional["vi-VN"] = {
-    closeText: "Đóng",
-    prevText: "Trước",
-    nextText: "Sau",
-    currentText: "Hôm nay",
-    monthNames: [
-      "Tháng một",
-      "Tháng hai",
-      "Tháng ba",
-      "Tháng tư",
-      "Tháng năm",
-      "Tháng sáu",
-      "Tháng bảy",
-      "Tháng tám",
-      "Tháng chín",
-      "Tháng mười",
-      "Tháng mười một",
-      "Tháng mười hai",
-    ],
-    monthNamesShort: [
-      "Một",
-      "Hai",
-      "Ba",
-      "Bốn",
-      "Năm",
-      "Sáu",
-      "Bảy",
-      "Tám",
-      "Chín",
-      "Mười",
-      "Mười một",
-      "Mười hai",
-    ],
-    dayNames: [
+/**
+ * Vietnamese translation for bootstrap-datepicker
+ * An Vo <https://github.com/anvoz/>
+ */
+(function ($) {
+  $.fn.datepicker.dates["vi"] = {
+    days: [
       "Chủ nhật",
       "Thứ hai",
       "Thứ ba",
@@ -41,15 +13,38 @@ jQuery(function ($) {
       "Thứ sáu",
       "Thứ bảy",
     ],
-    dayNamesShort: ["CN", "Hai", "Ba", "Tư", "Năm", "Sáu", "Bảy"],
-    dayNamesMin: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
-    weekHeader: "Tuần",
-    dateFormat: "dd/mm/yy",
-    firstDay: 1,
-    isRTL: false,
-    showMonthAfterYear: false,
-    yearSuffix: "",
+    daysShort: ["CN", "Thứ 2", "Thứ 3", "Thứ 4", "Thứ 5", "Thứ 6", "Thứ 7"],
+    daysMin: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
+    months: [
+      "Tháng 1",
+      "Tháng 2",
+      "Tháng 3",
+      "Tháng 4",
+      "Tháng 5",
+      "Tháng 6",
+      "Tháng 7",
+      "Tháng 8",
+      "Tháng 9",
+      "Tháng 10",
+      "Tháng 11",
+      "Tháng 12",
+    ],
+    monthsShort: [
+      "Th1",
+      "Th2",
+      "Th3",
+      "Th4",
+      "Th5",
+      "Th6",
+      "Th7",
+      "Th8",
+      "Th9",
+      "Th10",
+      "Th11",
+      "Th12",
+    ],
+    today: "Hôm nay",
+    clear: "Xóa",
+    format: "dd/mm/yyyy",
   };
-
-  $.datepicker.setDefaults($.datepicker.regional["vi-VN"]);
-});
+})(jQuery);
